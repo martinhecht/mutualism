21f6eee34fdc38aa98b60d25d989f6a957f07e10
martin <martin.hecht@uni-tuebingen.de>
2023-05-08 02:21:28 +0200

    0.0.10, bug fixes in gen.lavaan.syntax for RI-CLPM

--- R/gen.lavaan.syntax.R
+++ R/gen.lavaan.syntax.R
@@ -1,4 +1,5 @@
 ## Changelog:
+# MH 0.0.10 2023-05-08: bug fixes in RI-CLPM (first time point variance now free, b now not into first time point) and removed latent
 # MH 0.0.8 2022-10-14: initial programming
 
 ## Function definition
@@ -13,30 +14,41 @@ gen.lavaan.syntax <- function( model=c("CLPM","RI-CLPM"), T=2, t1.stationary=FAL
 				# RICLPM lavaan syntax
 				# https://jeroendmulder.github.io/RI-CLPM/lavaan.html
 				syn <- "# Create between components (random intercepts)"
-				syn <- c(syn,	paste0( "RIx =~", paste(paste0( "1*x.",1:T ), collapse=" + " ), "\n",
-										"RIy =~", paste(paste0( "1*y.",1:T ), collapse=" + " )  )
+				### MH 0.0.10 2023-05-08: now starting from second time point
+				syn <- c(syn,	paste0( "RIx =~", paste(paste0( "1*x.",2:T ), collapse=" + " ), "\n",
+										"RIy =~", paste(paste0( "1*y.",2:T ), collapse=" + " )  )
 							)
-				syn <- c(syn, "# Create within-person centered variables" )
-				for( t in 1:T ){
-					syn <- c(syn,	# latent
-									paste0( "fx.",t," =~ 1*x.",t, "\n",
-											"fy.",t," =~ 1*y.",t )
-							)
-				}
+				### MH 0.0.10 2023-05-08: removed
+				# syn <- c(syn, "# Create within-person centered variables" )
+				# for( t in 1:T ){
+					# syn <- c(syn,	# latent
+									# paste0( "fx.",t," =~ 1*x.",t, "\n",
+											# "fy.",t," =~ 1*y.",t )
+							# )
+				# }
 				syn <- c(syn, "# Estimate the lagged effects between the within-person centered variables" )
 				for( t in 2:T ){
-					syn <- c(syn,	paste0("fx.",t," ~ a1*fx.",t-1," + a12*fy.",t-1,"\n",
-										   "fy.",t," ~ a2*fy.",t-1," + a21*fx.",t-1   ),
-									paste0("fx.",t," ~~ evar1*fx.",t,"\n",
-										   "fy.",t," ~~ evar2*fy.",t,"\n",
-										   "fx.",t," ~~ ecov*fy.",t)
-									
+					### MH 0.0.10 2023-05-08: fx to x, fy to y
+					# syn <- c(syn,	paste0("fx.",t," ~ a1*fx.",t-1," + a12*fy.",t-1,"\n",
+										   # "fy.",t," ~ a2*fy.",t-1," + a21*fx.",t-1   ),
+									# paste0("fx.",t," ~~ evar1*fx.",t,"\n",
+										   # "fy.",t," ~~ evar2*fy.",t,"\n",
+										   # "fx.",t," ~~ ecov*fy.",t)
+					syn <- c(syn,	paste0("x.",t," ~ a1*x.",t-1," + a12*y.",t-1,"\n",
+										   "y.",t," ~ a2*y.",t-1," + a21*x.",t-1   ),
+									paste0("x.",t," ~~ evar1*x.",t,"\n",
+										   "y.",t," ~~ evar2*y.",t,"\n",
+										   "x.",t," ~~ ecov*y.",t)
 										   )
 				}
 				syn <- c(syn, paste0("# first time point\n",
-									 "fx.1 ~~ fvar1*x.1\n",
-									 "fy.1 ~~ fvar2*y.1\n",
-									 "fx.1 ~~ fcov*y.1\n",
+									 ### MH 0.0.10 2023-05-08: error: left fx, right x
+									 # "fx.1 ~~ fvar1*x.1\n",
+									 # "fy.1 ~~ fvar2*y.1\n",
+									 # "fx.1 ~~ fcov*y.1\n",
+									 "x.1 ~~ fvar1*x.1\n",
+									 "y.1 ~~ fvar2*y.1\n",
+									 "x.1 ~~ fcov*y.1\n",									 
 									 "# Estimate the variance and covariance of the random intercepts\n",
 									 "RIx ~~ RIxVar*RIx\n",
 									 "RIy ~~ RIyVar*RIy\n",
