f29a3532328abdb3ba83f570cb15ccf74459db9c
martinhecht <martin.hecht@hsu-hh.de>
2023-09-23 21:30:13 +0200

    0.0.15 2023-09-23: gen.lavaan.syntax: RI-CLPM, now with constraints for random process means, RI-CLPM2/3 discarded!!, parameter labels changed!!

--- R/gen.lavaan.syntax.R
+++ R/gen.lavaan.syntax.R
@@ -1,4 +1,5 @@
 ## Changelog:
+# MH 0.0.15 2023-09-23: RI-CLPM, now with constraints for random process means, RI-CLPM2/3 discarded!!
 # MH 0.0.14 2023-09-22: RI-CLPM3, intercept set to 1 for t >= 2, freely estimated parameter for intercept (=mean) at t=1
 # MH 0.0.11 2023-05-08: RI-CLPM2
 # MH 0.0.10 2023-05-08: bug fixes in RI-CLPM (first time point variance now free, b now not into first time point) and removed latent
@@ -7,83 +8,69 @@
 ## Function definition
 #### only F (number of processes) = 2 !!!
 #### t1.stationary: only FALSE !!!
-gen.lavaan.syntax <- function( model=c("CLPM","RI-CLPM","RI-CLPM2","RI-CLPM3"), T=2, t1.stationary=FALSE ){
+gen.lavaan.syntax <- function( model=c("CLPM","RI-CLPM"), T=2, t1.stationary=FALSE ){
+		
+		# cautionary note
+		if( model %in% "RI-CLPM" ) cat( "\nNote: Starting from version 0.0.15, the RI-CLPM model has changed (now with constraints for random process means)!\n      Be cautious when using old scripts that depend on versions <= 0.0.14\n\n" )
 		
 		syn <- NULL
-		
-		if( model %in% c("RI-CLPM","RI-CLPM2","RI-CLPM3") ){
+
+		if( model %in% c("RI-CLPM") ){
 			if( !t1.stationary ){
 				# RICLPM lavaan syntax
-				# https://jeroendmulder.github.io/RI-CLPM/lavaan.html
-				syn <- "# Create between components (random intercepts)"
-				### MH 0.0.10 2023-05-08: now starting from second time point
-				if( model %in% c("RI-CLPM") ){
-						syn <- c(syn,	paste0( "RIx =~", paste(paste0( "1*x.",2:T ), collapse=" + " ), "\n",
-												"RIy =~", paste(paste0( "1*y.",2:T ), collapse=" + " )  )
-								) } else if ( model %in% c("RI-CLPM2") ){
-						# MH 0.0.11 2023-05-08: RI-CLPM2				
-						syn <- c(syn,	paste0( "RIx =~1*x.1 + ", paste(paste0( "l1*x.",2:T ), collapse=" + " ), "\n",
-												"RIy =~1*y.1 + ", paste(paste0( "l2*y.",2:T ), collapse=" + " )  )
-								) } else if ( model %in% c("RI-CLPM3") ){
-						# MH 0.0.14 2023-09-22: RI-CLPM3
-						syn <- c(syn,	paste0( "RIx =~l1*x.1 + ", paste(paste0( "1*x.",2:T ), collapse=" + " ), "\n",
-												"RIy =~l2*y.1 + ", paste(paste0( "1*y.",2:T ), collapse=" + " )  )  )
-				}
+				syn <- "### Random intercepts cross-lagged panel model ###"
+				syn <- c( syn, "#   (random process means implementation)\n" )
+				# MH 0.0.15 2023-09-23: new RI-CLPM (with constraints for random process means)
+				syn <- c(syn, "# Random process means" )
+
+				syn <- c(syn, paste( c("mu1 =~ 1*x.1", paste0( "con1a*x.",2:T ), paste0( "con1b*y.",2:T )), collapse=" + " ),
+							  paste( c("mu2 =~ 1*y.1", paste0( "con2a*y.",2:T ), paste0( "con2b*x.",2:T )), collapse=" + " ) )
+				syn <- c(syn, c( "con1a == (1-a1)",
+								 "con2a == (1-a2)",
+								 "con1b == -1*a21",
+								 "con2b == -1*a12" ) )
 				
-				### MH 0.0.10 2023-05-08: removed
-				# syn <- c(syn, "# Create within-person centered variables" )
-				# for( t in 1:T ){
-					# syn <- c(syn,	# latent
-									# paste0( "fx.",t," =~ 1*x.",t, "\n",
-											# "fy.",t," =~ 1*y.",t )
-							# )
-				# }
-				syn <- c(syn, "# Estimate the lagged effects between the within-person centered variables" )
+				syn <- c(syn, paste0("# Random process means (co)variances\n",
+									 "mu1 ~~ mu1var*mu1\n",
+									 "mu2 ~~ mu2var*mu2\n",
+									 "mu1 ~~ mucov*mu2" )	)				
+				syn <- c(syn, paste0("# First time point (co)variances\n",
+									 "x.1 ~~ f1var*x.1\n",
+									 "y.1 ~~ f2var*y.1\n",
+									 "x.1 ~~ fcov*y.1" ) )
+				syn <- c(syn, "# Autoregressive and cross-lagged effects" )
 				for( t in 2:T ){
-					### MH 0.0.10 2023-05-08: fx to x, fy to y
-					# syn <- c(syn,	paste0("fx.",t," ~ a1*fx.",t-1," + a12*fy.",t-1,"\n",
-										   # "fy.",t," ~ a2*fy.",t-1," + a21*fx.",t-1   ),
-									# paste0("fx.",t," ~~ evar1*fx.",t,"\n",
-										   # "fy.",t," ~~ evar2*fy.",t,"\n",
-										   # "fx.",t," ~~ ecov*fy.",t)
 					syn <- c(syn,	paste0("x.",t," ~ a1*x.",t-1," + a12*y.",t-1,"\n",
-										   "y.",t," ~ a2*y.",t-1," + a21*x.",t-1   ),
-									paste0("x.",t," ~~ evar1*x.",t,"\n",
-										   "y.",t," ~~ evar2*y.",t,"\n",
-										   "x.",t," ~~ ecov*y.",t)
-										   )
+										   "y.",t," ~ a2*y.",t-1," + a21*x.",t-1 ) )
 				}
-				syn <- c(syn, paste0("# first time point\n",
-									 ### MH 0.0.10 2023-05-08: error: left fx, right x
-									 # "fx.1 ~~ fvar1*x.1\n",
-									 # "fy.1 ~~ fvar2*y.1\n",
-									 # "fx.1 ~~ fcov*y.1\n",
-									 "x.1 ~~ fvar1*x.1\n",
-									 "y.1 ~~ fvar2*y.1\n",
-									 "x.1 ~~ fcov*y.1\n",									 
-									 "# Estimate the variance and covariance of the random intercepts\n",
-									 "RIx ~~ RIxVar*RIx\n",
-									 "RIy ~~ RIyVar*RIy\n",
-									 "RIx ~~ RIxyCov*RIy" )	)
+				syn <- c(syn, "# Process error (co)variances" )
+				for( t in 2:T ){
+					syn <- c(syn,	paste0("x.",t," ~~ e1var*x.",t,"\n",
+										   "y.",t," ~~ e2var*y.",t,"\n",
+                                           "x.",t," ~~ ecov*y.",t ) )
+				}				
+
 				syn <- paste( syn, collapse="\n" )
-			
 			}
 		}
 		
 		if( model %in% "CLPM" ){
 			if( !t1.stationary ){		
 				# data analysis
-				syn <- "x.1 ~~ fvar1*x.1
-						y.1 ~~ fvar2*y.1
-						x.1 ~~ fcov*y.1"
+				syn <- paste0("# First time point (co)variances\n",
+							  "x.1 ~~ fvar1*x.1\n",
+							  "y.1 ~~ fvar2*y.1\n",
+							  "x.1 ~~ fcov*y.1")
+				syn <- c(syn, "# Autoregressive and cross-lagged effects" )
 				for( t in 2:T ){
 					syn <- c(syn,	paste0("x.",t," ~ a1*x.",t-1," + a12*y.",t-1,"\n",
-										   "y.",t," ~ a2*y.",t-1," + a21*x.",t-1   ),
-									paste0("x.",t," ~~ evar1*x.",t,"\n",
+										   "y.",t," ~ a2*y.",t-1," + a21*x.",t-1   ) )
+				}
+				syn <- c(syn, "# Process error (co)variances" )
+				for( t in 2:T ){
+					syn <- c(syn,	paste0("x.",t," ~~ evar1*x.",t,"\n",
 										   "y.",t," ~~ evar2*y.",t,"\n",
-										   "x.",t," ~~ ecov*y.",t)
-									
-										   )
+										   "x.",t," ~~ ecov*y.",t) )
 				}
 				syn <- paste( syn, collapse="\n" )
 			}
@@ -104,7 +91,8 @@ gen.lavaan.syntax <- function( model=c("CLPM","RI-CLPM","RI-CLPM2","RI-CLPM3"), 
 # }
 
 ### 
-# cat( gen.lavaan.syntax( "RI-CLPM", T=4 ) )
+# syn <- gen.lavaan.syntax( "RI-CLPM", T=3 )
+# cat(syn)
 # cat( gen.lavaan.syntax( "RI-CLPM3", T=4 ) )
 # cat( gen.lavaan.syntax( "CLPM", T=4 ) )
 
